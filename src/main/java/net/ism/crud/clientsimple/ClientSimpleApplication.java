dff32d743a3e7ff05d180a48e4e32b8ec87e54fc
Ian Melo <ia.nm.santos@hotmail.com>
2019-08-13 03:55:20 -0300

    Primeiro commit

---
+++ src/main/java/net/ism/crud/clientsimple/ClientSimpleApplication.java
@@ -0,0 +1,13 @@
+package net.ism.crud.clientsimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientSimpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/ism/crud/clientsimple/ClientSimpleApplicationTests.java
@@ -0,0 +1,16 @@
+package net.ism.crud.clientsimple;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClientSimpleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
